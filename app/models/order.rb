d7a8a1d5948322eb6e2634da49283df93e6899b6
redouane <redouaanehafid@gmail.com>
2019-01-10 17:17:55 +0100

    first commit

---
+++ app/models/order.rb
@@ -0,0 +1,10 @@
+class Order < ApplicationRecord
+  
+  belongs_to :cart
+
+  enum status: [:ordered, :payed, :in_progress, :sent]
+
+  validates :name, :address, :email, presence: true
+  validates :status, inclusion: statuses.keys
+
+end
